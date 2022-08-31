01fa40629348e325c78aedc79cd10318ad927b8f
ttytang <267282100@qq.com>
2022-08-31 11:22:24 +0800

    initial version of homework TA for python

---
+++ hwta.py
@@ -0,0 +1,331 @@
+#########example code############
+###def square(x):
+###    '''
+###    Squares a number and returns the result.
+###    >>> square(2)
+###    4
+###    >>> square(3)
+###    9
+###    '''
+###    'Below is your coding area'
+###    return x**x
+###
+###if __name__=='__main__':
+###    import doctest, hwta
+###    doctest.testmod(hwta)
+
+import logging
+import sys
+import os
+import re
+import openpyxl
+import json
+from configparser import ConfigParser
+
+class ReadConfig:
+    def __init__(self, filepath=None):
+        if filepath:
+            configPath = filepath
+        else:
+            curPath = os.path.dirname(os.path.realpath(sys.argv[0]))
+            configPath = os.path.join(curPath, "config.ini")
+        self.cf = ConfigParser()
+        self.cf.read(configPath, encoding="utf-8-sig")
+
+    def datapath(self, param):
+        value = self.cf.get("datapath", param)
+        return value
+
+    def mask(self, param):
+        value = self.cf.get("mask", param)
+        return value
+
+    def question(self, param):
+        value = self.cf.get("question", param)
+        return value
+
+    def excel(self, param):
+        value = self.cf.get("excel", param)
+        return value
+
+class CustomFormatter(logging.Formatter):
+   '''Logging Formatter to add colors and count warning / errors'''
+   grey = '\x1b[38;21m'
+   yellow = '\x1b[33;21m'
+   red = '\x1b[31;21m'
+   bold_red = '\x1b[31;1m'
+   reset = '\x1b[0m'
+   format = '%(asctime)s - %(message)s (%(filename)s:%(lineno)d) - %(name)s - %(levelname)s' 
+   
+   FORMATS = {
+       logging.DEBUG: grey+format+reset,
+       logging.INFO: grey+format+reset,
+       logging.WARNING: yellow+format+reset,
+       logging.ERROR: red+format+reset,
+       logging.CRITICAL: bold_red+format+reset
+   }
+
+   def format(self, record):
+       log_fmt = self.FORMATS.get(record.levelno)
+       formatter = logging.Formatter(log_fmt)
+       return formatter.format(record)
+
+class Grader():
+    def __init__(self, classname,configFile):
+        self.base_path = os.getcwd()
+        homework_ind = configFile.question("homework_ind")
+        self.source_path = configFile.datapath("source_path") + '/' + classname + '/' + homework_ind#let grader know where to find the source homework
+        self.bak_path = configFile.datapath("bak_path") + '/' + classname + '/' + homework_ind
+        self.paper_path = classname + '/' + configFile.datapath("paper_path") + '/' + homework_ind
+        self.question_path = configFile.datapath("question_path")#let grader know where to find the question bank
+        self.mark_path = classname + '/' + configFile.datapath("mark_path") + '/' + homework_ind
+        self.wrap_path = classname + '/' + configFile.datapath("wrap_path")
+        self.xlsx_path = classname + '/' + configFile.datapath("xlsx_path")
+        self.question_ind = configFile.question('question_ind')
+        self.id_status = self.wrap_path + '/' + 'id_status.json'
+        self.class_id = classname
+        self.cur_stu_id = '0'
+        self.cur_hw_id = homework_ind
+        self.hw_status = {}
+        self.id_col = configFile.excel('id_col')
+        self.score_col = configFile.excel('score_col')
+        self.qb_start_pos = int(configFile.question('question_body_start_pos'))
+        self.qb_end_pos = int(configFile.question('question_body_end_pos'))
+    
+    def make_fs(self):
+        #os.makedirs(self.source_path, exist_ok=True) #preconstructed outside the Grader
+        #os.makedirs(self.questions_path, exist_ok=True) #preconstructed outside the Grader
+        os.makedirs(self.bak_path, exist_ok=True)
+        os.makedirs(self.paper_path, exist_ok=True)
+        os.makedirs(self.mark_path, exist_ok=True)
+        os.makedirs(self.wrap_path, exist_ok=True)
+        os.makedirs(self.xlsx_path, exist_ok=True)
+        if not os.path.exists(self.xlsx_path + '/' + self.class_id + '.xlsx'):
+            mv_xlsx_cmd = 'cp' + ' ' + '-rf' + ' ' + self.class_id + '.xlsx' + ' ' + self.xlsx_path
+            os.system(mv_xlsx_cmd)
+        if not os.path.exists(self.wrap_path+'/'+'suffix.py'):
+            ln_suffix_cmd = 'ln' + ' ' + os.getcwd() + '/' + self.question_path + '/' + 'suffix.py' + ' ' + self.wrap_path
+            os.system(ln_suffix_cmd)
+        if os.path.exists(self.id_status):
+            with open(self.id_status, 'r') as f:
+                self.id_status = json.load(f)
+        else:
+            self.id_status = {}#eg:{20220901001:[1,3,1],20220901002:[1,4,0],20220901003:[0,0,0]}//{学号:[marked_flag,score,plagiarized_flag]} 
+    
+    def checked_in_xlsx(self, content, xlsx_file, region):
+        wb = openpyxl.load_workbook(xlsx_file)
+        ws = wb.active
+        #if content in [item.value for item in ws[region]]:
+        #    return True
+        #else:
+        #    return False
+        ind = [i for i, item in enumerate(ws[region]) if item.value==content]
+        if ind:
+            self.id_status[content]=[[region, str(ind[0]+1)]]#Notes: 1:'+1' due to excel row is from 1 to start;2:[()] means the structure is list and the first elment is tuple
+            #self.id_status[content].append(0)#the second position is for marked_flag, initial value is 0: not marked yet
+            return True
+        else:
+            return False
+    
+    def fetch_paper(self):
+        mod_cmd = 'chmod 700 -R %s' % self.source_path
+        os.system(mod_cmd)
+        bak_src_cmd = 'rsync -at %s/*.py %s/' % (self.source_path, self.bak_path)
+        os.system(bak_src_cmd)
+        s_list = os.listdir(self.source_path)
+        got_paper = False
+        for s in s_list:
+            s_id = re.match(r'(\d{10}).*\.py$', s)#get s_id from paper name
+            if not s_id:
+                continue
+            s_id = s_id.group(1)
+            s_id_hw = self.paper_path + '/' + s
+            if not os.path.exists(s_id_hw) and self.checked_in_xlsx(s_id, self.xlsx_path+'/'+self.class_id+'.xlsx', self.id_col): #put in self.paper_path if s_id in excel and not in self.paper_path
+                mv_src_cmd = 'mv' + ' ' + self.source_path + '/' + s + ' ' + self.paper_path
+                os.system(mv_src_cmd)
+                self.id_status[s_id].append(0)  # the second position is for marked_flag, initial value is 0: not marked yet
+                got_paper = True
+        return got_paper
+                
+    def get_assign(self):
+        a_dict = {}
+        self.hw_status[self.cur_hw_id]={}
+        q_str = self.question_ind
+        q_list = q_str.split(',')
+        q_files = [q+'.py' for q in q_list]
+        for q in q_files:
+            num = 0
+            q_name = self.question_path + '/' + q
+            with open (q_name, 'r') as f:
+                prefix = f.readlines()
+            a_dict[prefix[self.qb_start_pos]]=prefix[self.qb_start_pos:self.qb_end_pos]
+            for p in prefix[self.qb_start_pos:self.qb_end_pos]:
+                if p[4:8]=='>>> ':
+                    num += 1
+            q_ind = q.split('.')[0]
+            self.hw_status[self.cur_hw_id][q_ind] = num
+        return(a_dict)
+        
+    def marked_checker(self, f): #check if marked already
+        stu_id = re.match(r'(\d{10}).*\.py$', f).group(1)
+        self.cur_stu_id = stu_id
+        #if (not stu_id in self.id_status):
+            ##self.id_status[stu_id]=[0]
+            #self.id_status[stu_id].append(0)
+        #return self.id_status[stu_id][0]
+        return self.id_status[stu_id][1]#1 means marked_flag position, while 0 is the position of (col,row) in excel
+        
+    def wrapper(self, paper, items, wrapfile):
+        with open(paper, 'r') as fr:
+            paper_code = fr.readlines()
+        assign_set = set(items.keys())
+        with open(wrapfile,'w') as fw:
+            for code_line in paper_code:
+                if code_line in assign_set:
+                    assign_set.remove(code_line)
+                    #finished_item_num += 1
+                    doctest_line = items[code_line]
+                    for cl in doctest_line:
+                        fw.write(cl)
+                else:
+                    fw.write(code_line)
+            fw.flush()
+
+    def tester(self):
+        test_path = self.wrap_path
+        os.chdir(test_path)
+        run_case_cmd = 'python3 suffix.py'
+        fh = os.popen(run_case_cmd, 'r')
+        buff = fh.readlines()
+        os.chdir(self.base_path)
+        return (buff)
+
+    def marker(self, result):
+        cp_cmd = 'cp' + ' ' + '-rf' + ' ' + self.paper_path + '/' + self.cur_stu_id + '.py' + ' ' + self.mark_path + '/'
+        os.system(cp_cmd)
+        #self.id_status[self.cur_stu_id] = [1]
+        self.id_status[self.cur_stu_id][1] = 1
+        stu_marked_paper = self.mark_path + '/' + self.cur_stu_id + '.py'
+        write_num = 0
+        with open(stu_marked_paper, 'a') as m:
+            m.write('\n\n\n')
+            m.write('**********Below is the comment region***********\n')
+            if not result:
+                self.id_status[self.cur_stu_id].append(100.0)
+                m.write('Passed all the test cases, score is: %.0f! \n' % self.id_status[self.cur_stu_id][2])
+                m.write('Congratulations!')
+            else:
+                for line in result:
+                    if(write_num):
+                        m.write(line)
+                        write_num -= 1
+                        continue
+                    if line == 'Failed example:\n':
+                        m.write(line)
+                        write_num = 1
+                        continue
+                    if line == 'Exception raised:\n':
+                        m.write(line)
+                        continue
+                    if re.match(r'\s+\w+Error.+', line):
+                        m.write(line)
+                        continue
+                    if line == 'Expected:\n':
+                        m.write(line)
+                        write_num= 1
+                        continue
+                    if line == 'Got:\n':
+                        m.write(line)
+                        write_num = 1
+                        continue
+                    if re.match(r'(\d+) items had failures:\n', line):
+                        error_items = re.match(r'(\d+) items had failures:\n', line).group(1)
+                        error_items = int(error_items)
+                        m.write('\n')
+                        m.write('%d items had failures out of %d\n' % (error_items, len(self.hw_status[self.cur_hw_id])))
+                        write_num = error_items
+                        continue
+                    if re.match(r'.+\d+ failures\.\n', line):
+                        error_cases = re.match(r'.+(\d+) failures\.\n', line).group(1)
+                        error_cases = int(error_cases)
+                        m.write('%d cases had failures out of %d\n' % (error_cases, sum(self.hw_status[self.cur_hw_id].values())))
+                        score = round(-error_cases/sum(self.hw_status[self.cur_hw_id].values())*40+100,0)
+                        self.id_status[self.cur_stu_id].append(score)
+                        m.write('\n')
+                        m.write('Score is: %.0f' % score)
+                        continue
+                    if line == 'SyntaxError\n':
+                        m.write(line)
+                        m.write('\n')
+                        score = 59.0
+                        self.id_status[self.cur_stu_id].append(score)
+                        m.write('Score is %.0f' % score)
+                        continue
+                    if line == 'IndentationError\n':
+                        m.write(line)
+                        m.write('\n')
+                        score = 59.0
+                        self.id_status[self.cur_stu_id].append(score)
+                        m.write('Score is %.0f' % score)
+                        continue
+
+    def recoder(self):
+        with open(self.wrap_path + '/' + 'id_status.json', 'w') as fid:
+            json.dump(self.id_status, fid, indent=4, sort_keys=True)
+        wb = openpyxl.load_workbook(self.xlsx_path+'/'+self.class_id+'.xlsx')
+        ws = wb.active
+        for sidv in self.id_status.values():
+            cr = self.score_col+sidv[0][1]
+            ws[cr]=sidv[2]
+        wb.save(self.xlsx_path+'/'+self.class_id+'.xlsx')
+
+
+
+    def liner(self):
+        self.make_fs()    
+        got_paper = self.fetch_paper()#from source_path to clean into paper_path and copy to bak_path
+        if not got_paper:
+            return False
+        assign_items = self.get_assign()#return dictionary such as {'def square():': content, 'def sqrt():':content, 'def sum():':content}
+        p_list = os.listdir(self.paper_path)
+        for p in p_list:
+            #finished_item_num = 0
+            marked = self.marked_checker(p)#check if not test due to late submission, set id_status dict and mkdir hwx
+            if not marked:
+                paper_file = self.paper_path + '/' + p
+                wrap_file = self.wrap_path + '/' + 'test.py'
+                self.wrapper(paper_file, assign_items, wrap_file)#input: p, self.questions_path, self_questions_list and self_exec_path. insert docstring and __main__ to generate executable python
+                stdout_buff = self.tester()#input: p_wraped, self.marks_path. run and mark operation to generate marked paper
+                self.marker(stdout_buff)
+        self.recoder()#input: self.id_status and self.excel_path
+        return True
+            
+    def cleanup(self):
+        mod_cmd = 'chmod 775 -R %s' % self.source_path
+        os.system(mod_cmd)
+
+if __name__=='__main__':
+    #create logger with 'spam_application'
+    logger = logging.getLogger('HwTA')     
+    #create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)    
+    if len(sys.argv)>1:
+        if sys.argv[1] == 'color':
+            ch.setFormatter(CustomFormatter())    
+    if len(sys.argv)>2:
+        logger.setLevel(logging.__getattribute__(sys.argv[2]))
+    else:
+        logger.setLevel(logging.DEBUG)    
+    logger.addHandler(ch)
+        
+    conf = ReadConfig()
+    class_list = conf.datapath('class_path')
+    class_list = class_list.split(',')
+    for cls in class_list:
+        os.makedirs(cls, exist_ok=True)
+        graderTA = Grader(cls, conf)
+        graderTA.liner()
+        print('finished %s class' % cls)
+        graderTA.cleanup()#rm source_path file and set source_path rw right to 775
+    
